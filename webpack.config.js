0e2ae59767b7a184d729ebbcdc134ffb509418cd
Chris Closser <chrisclosser1@gmail.com>
2017-06-14 13:20:57 -0700

    replaced -p flag on post build

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const CleanPlugin = require('clean-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
 
 dotenv.load();
 
@@ -19,17 +19,17 @@ const plugins = [
   }),
 ];
 
-// if(production) {
-//   plugins = plugins.concat([
-//     new webpack.optimize.UglifyJsPlugin({
-//       mangle: true,
-//       compress: {
-//         warnings: false,
-//       },
-//     }),
-//     new CleanPlugin(),
-//   ]);
-// }
+if(production) {
+  plugins = plugins.concat([
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new CleanPlugin(),
+  ]);
+}
 
 module.exports = {
   entry: `${__dirname}/app/entry.js`,
